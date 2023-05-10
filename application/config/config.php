d63a8b32caeddcb97af15eab2bbe89fd2cc753af
siledth <siledth@gmail.com>
2023-05-10 13:25:41 -0400

    first commit9

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://192.168.250.18/caribia';
-$config['base_url'] = 'http://localhost/caribia';
+$config['base_url'] = 'http://192.168.250.26/caribia';
+//$config['base_url'] = 'http://localhost/caribia';
 /*
 |--------------------------------------------------------------------------
 | Index File
