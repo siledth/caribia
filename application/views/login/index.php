2db952d23084d19689f1de418a17e1ef543644e1
siledth <siledth@gmail.com>
2023-05-10 13:21:59 -0400

    first commit8

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -81,7 +81,7 @@ $captcha_numbers = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);*/
                         </span>
                         <b class="ml-3"> Bienvenido</b>
                     </div>
-                    <div class="icon">
+                    <div class="icon"> 
                     
                      
                     <i class="fa fa-sign-in"></i>
